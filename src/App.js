c9385be0715fb56e41466cb9cf22c4bd60277beb
Johan Swan <jswannieman@gmail.com>
2023-12-31 10:55:23 +1300

    Update App JS Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Score from "./Score";
 import "./App.css";
 
 function App() {
-  return <div className="App">HALLO</div>;
+  return (
+    <div className="App">
+      <Score />
+    </div>
+  );
 }
 
 export default App;
