eb31e5a099e9adee70f3e75af5d629ff9b8d576e
Johan Swan <jswannieman@gmail.com>
2023-12-31 11:14:31 +1300

    Update Score JSX Component

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Score = () => {
-  return <div>Score</div>;
+  const [score, setScore] = useState(localStorage.getItem("currentScore") || 0);
+
+  useEffect(() => {
+    localStorage.setItem("currentScore", score);
+  }, [score]);
+  return (
+    <div>
+      <h2>Your Current Score Is {score}</h2>
+      <button onClick={() => setScore((prevaScore) => prevaScore + 1)}>
+        Add 1 to score
+      </button>
+      <button onClick={() => setScore((prevbScore) => prevbScore - 1)}>
+        Subtract 1 from score
+      </button>
+    </div>
+  );
 };
 
 export default Score;
